7e0f2ad496f909f97eb1c6cfade20f656421d62c
Rokiatou20 <rouattar15@gmail.com>
2023-02-15 16:48:26 +0000

    Determine the winner

--- square-game/src/components/Board.js
+++ square-game/src/components/Board.js
@@ -5,39 +5,86 @@ class Board extends React.Component {
     // The state contains a boolean to know who is the active player
     // True => First player is active AND False => Second player is active 
     state = {
-        isFirstPlayer: true
+        isFirstPlayer: true,
+        squares: Array(9).fill(null)
+
     }
     
-    // Function to active next player
-    activeSecondPlayer = () => this.setState({isFirstPlayer: !this.state.isFirstPlayer})
+    handleClick = i => {
+        const squares = this.state.squares.slice()
 
-    renderSquare() {
-        return <Square isFirstPlayer={this.state.isFirstPlayer} activeSecondPlayerProps={this.activeSecondPlayer}/>;
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        
+        squares[i] = this.state.isFirstPlayer ? 'X' : 'O'
+        this.setState({
+            squares: squares,
+            isFirstPlayer: !this.state.isFirstPlayer
+        })  
+        console.log(squares)
+    }
+
+    renderSquare(i) {
+        return (
+            <Square 
+                value = {this.state.squares[i]} 
+                onClick = {() => this.handleClick(i)} 
+            />
+        )
     }
 
     render() {
-        const status = 'Next player: X';
+        let status;
+        const winner = calculateWinner(this.state.squares)
+        
+        if (winner)
+            status = "Winner: " + winner 
+        else
+            status = "Next Player: " + ( this.state.isFirstPlayer ? 'X' : 'O')
 
         return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                {this.renderSquare()}
-                {this.renderSquare()}
-                {this.renderSquare()}
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-                {this.renderSquare()}
-                {this.renderSquare()}
-                {this.renderSquare()}
-            </div>
-            <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-            </div>
-        </div>
         );
     }
 }
+
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
 export default Board;
--- square-game/src/components/Square.js
+++ square-game/src/components/Square.js
@@ -1,33 +1,14 @@
 import React from "react";
 
-class Square extends React.Component {
+const Square = ({value, onClick}) => { 
 
-    state = {
-        value: ''
-    }
+    return (
+        <button className="square" onClick={onClick}
+        >
+            {value}
+        </button>
+    );
 
-    
-    
-    changeValue = () => {
-        console.log(this.props)
-        if (this.props.isFirstPlayer) {
-            this.setState({value: 'X'})
-        }
-        else {
-            this.setState({value: 'O'})
-        }
-        this.props.activeSecondPlayerProps()
-    }
-
-    render() {
-        
-
-        return (
-            <button className="square" onClick={this.changeValue}>
-                {this.state.value}
-            </button>
-        );
-    }
 }
 
 export default Square;
